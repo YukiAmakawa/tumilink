8cd379283eba9e8ac5f93c7b40226dc66f37a36e
AmakawaYuki <amakawa.yuki@prevent.co.jp>
2020-06-28 21:28:34 +0900

    add:検索フォーム

---
+++ components/atoms/form/Apptext.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="widthClass" class="FormText FormAtom">
+    <input
+      :id="id ? id : ''"
+      v-model="inputText"
+      :type="type"
+      :placeholder="placeholder ? placeholder : ''"
+      :disabled="disabled"
+      :pattern="pattern"
+      class="block w-full text-input"
+    >
+  </div>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AppText',
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: [String, Number],
+      default: '',
+      required: true
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    pattern: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    inputText: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.AppText {
+  .text-input {
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    /* padding: 12px; */
+    min-height: 40px;
+    border-radius: 3px;
+    /* background-color: var(--white); */
+    &:focus {
+      border: 1px solid #3092e7;
+    }
+  }
+  .text-input::placeholder {
+    /* font-size: var(--text-md); */
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        tumilink
-      </h1>
-      <h2 class="subtitle">
-        {{ name }}
-      </h2>
-      <div v-if="isLineClient" class="links">
-        <div @click="sendMessage">
-          送信
-        </div>
-      </div>
-      <p>新しい部分</p>
-      <li v-for="content in contents" :key="content">
+  <div class="wrap">
+    <AppSearch
+      v-model="searchText"
+      :handleSearch="handleSearch"
+    />
+    <div v-if="isLineClient" class="links">
+      <li v-for="(content, index) in filteredContents" :key="index">
         {{ content.url }}
       </li>
 
@@ -24,18 +15,19 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import AppSearch from '@/components/atoms/form/AppSearch.vue'
 export default {
   components: {
-    Logo
+    AppSearch
   },
   data() {
     return {
       name: '',
       contents: [],
+      filteredContents: [],
       lineId: '',
-      error: ''
+      error: '',
+      searchText: ''
     }
   },
   async created() {
@@ -74,57 +66,38 @@ export default {
       try {
         const { data } = await this.$axios.get(`${process.env.API_URL}api/users/1`)
         this.contents = data.response.contents
+        this.filteredContents = data.response.contents
       } catch (e) {
         this.error = e
       }
     },
-    async sendMessage() {
-      try {
-        await window.liff
-          .sendMessages([
-            {
-              type: 'text',
-              text: '送信が完了しました'
-            }
-          ])
-        window.liff.closeWindow()
-      } catch (e) {
-        window.alert('Error sending message: ' + e)
+    handleSearch() {
+      if (!this.contents) return
+      if (!this.searchText) {
+        this.filteredContents = this.contents
+      } else {
+        this.filteredContents = this.contents.filter((content) => {
+          return content.url.includes(this.searchText)
+        })
       }
     }
+    // async sendMessage() {
+    //   try {
+    //     await window.liff
+    //       .sendMessages([
+    //         {
+    //           type: 'text',
+    //           text: '送信が完了しました'
+    //         }
+    //       ])
+    //     window.liff.closeWindow()
+    //   } catch (e) {
+    //     window.alert('Error sending message: ' + e)
+    //   }
+    // }
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
